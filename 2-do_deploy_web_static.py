febc6de705371c8ac80dc14245b506b1db7d2173
root <abdelwahab.hamada@gmail.com>
2023-12-07 21:44:29 -0800

    index.html

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,7 @@ archive to the web servers
 from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['52.3.244.177', '100.26.152.208']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):

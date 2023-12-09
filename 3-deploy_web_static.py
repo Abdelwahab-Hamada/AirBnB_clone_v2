f721442492b5052d2950f18aed90f336f0817627
root <abdelwahab.hamada@gmail.com>
2023-12-08 23:37:05 -0800

    sudo

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,6 +7,7 @@ from os.path import isdir
 from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['52.3.244.177', '100.26.152.208']
+env.user = "ubuntu"
 
 
 def do_pack():
@@ -33,13 +34,13 @@ def do_deploy(archive_path):
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('sudo mkdir -p {}{}/'.format(path, no_ext))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('sudo rm /tmp/{}'.format(file_n))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('sudo rm -rf {}{}/web_static'.format(path, no_ext))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         print("New version deployed!")
         return True
     except:

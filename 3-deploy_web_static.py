b457ec6dc0b64322db99b41f5319d8fb9d545512
root <abdelwahab.hamada@gmail.com>
2023-12-08 22:52:13 -0800

    full

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,6 +4,9 @@
 from datetime import datetime
 from fabric.api import local
 from os.path import isdir
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['52.3.244.177', '100.26.152.208']
 
 
 def do_pack():
@@ -17,3 +20,32 @@ def do_pack():
         return file_name
     except:
         return None
+
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
+
+
+def deploy():
+    """Full deploy"""
+    archive_path = do_pack()
+    if archive_path:
+        return do_deploy(archive_path)
+    return False

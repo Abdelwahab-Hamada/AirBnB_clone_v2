18ef4567800151d87f4b5a225f66236911c42d4a
root <abdelwahab.hamada@gmail.com>
2023-12-30 22:51:37 -0800

    9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -19,6 +19,7 @@ def states_list(id=None):
     """
     path = '9-states.html'
     states = storage.all(State)
+    print(states.get('State.'+id))
     return render_template(path, states=states, id=id)
 
 
